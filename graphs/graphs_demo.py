97394ebe2d7ddda6b90649efd7cd93d3df709abc
caffwin <catheralligator@gmail.com>
2019-01-09 04:08:46 +0000

    graphs exercise

---
+++ graphs/graphs_demo.py
@@ -0,0 +1,64 @@
+class Queue(object):
+    def __init__(self):
+        self.items = []
+        
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.pop(0)
+
+    def is_empty(self):
+        return self.items == []
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+class HumanNode(object):
+
+    def __init__(self, name, adjacent=None):
+        if adjacent is None:
+            adjacent = set()
+
+        assert isinstance(adjacent, set)
+        self.name = name
+        self.adjacent = adjacent
+
+class FriendGraph(object):
+
+    def __init__(self):
+
+        self.nodes = set()
+
+    def add_person(self, person):
+        self.nodes.add(person)
+
+    def set_friends(self, person1, person2):
+
+        person1.adjacent.add(person2)
+        person2.adjacent.add(person1)
+
+    def are_connected(self, person1, person2):
+        # BFS using queue
+        possible_nodes = Queue()
+        seen = set()
+        possible_nodes.enqueue(person1)
+        seen.add(person1)
+
+        while not possible_nodes.is_empty():
+            person = possible_nodes.dequeue()
+            if person is person2:
+                return True
+            else:
+                for friend in person.adjacent - seen:
+                    possible_nodes.enqueue(friend)
+                    seen.add(friend)
+                    print("Added to queue: ", friend)
+        return False
+
